0475f5fdcfab094114aab8dabb1220556616c2d0
azelionni@gmail.com <azelionni@gmail.com>
2021-11-29 18:48:34 +0200

    add Collection from apache

--- src/main/java/base/Main.java
+++ src/main/java/base/Main.java
@@ -1,19 +1,21 @@
 package base;
 
 import com.google.gson.Gson;
+import org.apache.commons.collections4.BidiMap;
+import org.apache.commons.collections4.SortedBidiMap;
+import org.apache.commons.collections4.bidimap.DualHashBidiMap;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.Optional;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
+
         testJsonOne();
 
         Collection<User> users = new LinkedList<>();
         UsersFactory create = new UsersFactory();
-        Collection <String> userInGson = new ArrayList<>();
+        Collection<String> userInGson = new ArrayList<>();
         Collection<User> backUser = new ArrayList<>();
         String temporally;
         User returnedUser;
@@ -24,14 +26,33 @@ public class Main {
             users.add(create.create());
         }
         for (User user : users) {
-           temporally = gson.toJson(user);
+            temporally = gson.toJson(user);
             userInGson.add(temporally);
         }
         for (String s : userInGson) {
             returnedUser = gson.fromJson(s, User.class);
             backUser.add(returnedUser);
         }
+        //Test  apache collection
+        BidiMap<String, User> bidiMap = new DualHashBidiMap<>();
+        bidiMap.put("one", create.create());
+        bidiMap.put("two", create.create());
+        bidiMap.put("three", create.create());
+        System.out.println("normal:");
+        for (String s : bidiMap.keySet()) {
+            System.out.println(bidiMap.get(s) + " key: " + s);
+        }
+       Map<User, String> inverseBidiMapverted = bidiMap.inverseBidiMap();
+        System.out.println("inverse:");
+        for (User user : inverseBidiMapverted.keySet()) {
+            System.out.println(inverseBidiMapverted.get(user) + " key " + user);
+        }
+
+        bidiMap.put("one", create.create());
+        bidiMap.put("two", create.create());
+        bidiMap.put("three", create.create());
     }
+
     private static void testJsonOne() {
         User account = new User("Vasyl", null, 19, true);
         Gson son = new Gson();
@@ -50,7 +71,7 @@ public class Main {
         String size = args[0];
         try {
             length = Integer.parseInt(size);
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             System.out.println(" some exception");
             return null;
         }
